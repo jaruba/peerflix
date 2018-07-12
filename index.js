a1f87d41713d8c111a5f2f4e2936f7d58f2dcd12
Jaruba Firanelli <jaruba@Jarubas-MacBook-Pro.local>
2018-07-12 15:25:38 +0300

    Add Destroy Method to Server

--- index.js
+++ index.js
@@ -62,6 +62,22 @@ var createServer = function (e, opts) {
   if (e.torrent) onready()
   else e.on('ready', onready)
 
+  var connections = {}
+
+  server.on('connection', function(conn) {
+    var key = conn.remoteAddress + ':' + conn.remotePort;
+    connections[key] = conn;
+    conn.on('close', function() {
+      delete connections[key];
+    });
+  });
+
+  server.destroy = function(cb) {
+    server.close(cb);
+    for (var key in connections)
+      connections[key].destroy();
+  };
+
   server.on('request', function (request, response) {
     var u = url.parse(request.url)
     u.pathname = decodeURIComponent(u.pathname)
